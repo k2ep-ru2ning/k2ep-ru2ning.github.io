7e869025008c4ed08491950b7f7b8ca2b43d151f
k2ep-ru2ning <rookie2000g@gmail.com>
2024-06-25 21:57:19 +0900

    feat: logo link 컴포넌트 다크테마 반영해서 스타일링

--- src/app/_component/logo-link.tsx
+++ src/app/_component/logo-link.tsx
@@ -4,7 +4,7 @@ export default function LogoLink() {
   return (
     <Link
       href="/"
-      className="flex flex-col text-center divide-y-2 divide-green-500 p-2 hover:bg-green-50 rounded-md"
+      className="flex flex-col text-center divide-y-2 divide-green-500 p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 text-gray-950 dark:text-gray-50"
     >
       <h1 className="font-bold text-3xl">{"Dev-Story"}</h1>
       <strong className="font-normal">{"k2ep-ru2ning"}</strong>
