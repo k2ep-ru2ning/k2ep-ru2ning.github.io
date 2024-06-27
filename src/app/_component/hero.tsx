eb63f77c85295a4d402f2be231b3afa09b098d5b
k2ep-ru2ning <rookie2000g@gmail.com>
2024-06-27 23:00:09 +0900

    chore: 메인 컬러를 green에서 indigo로 수정

--- src/app/_component/hero.tsx
+++ src/app/_component/hero.tsx
@@ -2,8 +2,8 @@ export default function Hero() {
   return (
     <section className="text-lg p-4 text-center text-gray-950 dark:text-gray-50">
       <p>
-        프로그래밍 관련 <strong className="text-green-500">지식/경험</strong>을{" "}
-        <strong className="text-green-500">기록/회고</strong>
+        프로그래밍 관련 <strong className="text-indigo-500">지식/경험</strong>을{" "}
+        <strong className="text-indigo-500">기록/회고</strong>
         하는 공간,
       </p>
       <h2 className="font-bold">{"Dev-Story"}</h2>
