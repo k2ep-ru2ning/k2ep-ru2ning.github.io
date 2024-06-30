7305cb7a9e2f23579d3d66db67476d56ca8b9b2b
k2ep-ru2ning <rookie2000g@gmail.com>
2024-06-30 15:33:16 +0900

    chore: 디렉터리 구조 변경 - src 디렉터리 제거

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   darkMode: "selector",
-  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
