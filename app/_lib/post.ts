fc895f957e2a576ec85a55e04f21732568846149
k2ep-ru2ning <rookie2000g@gmail.com>
2024-07-02 23:57:15 +0900

    feat: post 디렉터리에 있는 모든 mdx 파일을 읽어서 post 목록을 반환하는 getPosts 함수 구현

---
+++ app/_lib/post.ts
@@ -0,0 +1,43 @@
+import path from "path";
+import matter from "gray-matter";
+import { glob } from "glob";
+import { readFile } from "fs/promises";
+
+type PostMatter = {
+  title: string;
+  description: string;
+  createdAt: Date;
+};
+
+export type Post = PostMatter & {
+  slug: string;
+  content: string;
+};
+
+const POSTS_DIRECTORY_PATH = path.resolve(process.cwd(), "posts");
+
+async function getPostPaths(): Promise<string[]> {
+  return glob(`${POSTS_DIRECTORY_PATH}/**/*.mdx`);
+}
+
+export async function getPosts(): Promise<Post[]> {
+  const posts: Post[] = [];
+  try {
+    const postPaths = await getPostPaths();
+    for (const postPath of postPaths) {
+      const file = await readFile(postPath, { encoding: "utf8" });
+      const { content, data } = matter(file);
+      const { createdAt, description, title } = data as PostMatter;
+      posts.push({
+        content,
+        createdAt,
+        description,
+        title,
+        slug: postPath.slice(POSTS_DIRECTORY_PATH.length).replace(".mdx", ""),
+      });
+    }
+  } catch (e) {
+    throw new Error("post 파일을 read, parse 하는데 문제가 발생했습니다.");
+  }
+  return posts;
+}
