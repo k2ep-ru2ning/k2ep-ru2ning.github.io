087e6ba1f85ac1eccf08b1768596997d4ef638aa
k2ep-ru2ning <rookie2000g@gmail.com>
2024-07-07 16:06:56 +0900

    chore: 최신 글 아이템 컴포넌트 스타일링 (hover 시)

--- app/_component/recent-post-list-item.tsx
+++ app/_component/recent-post-list-item.tsx
@@ -10,14 +10,21 @@ export default function RecentPostListItem({
   post: { title, description, createdAt, slug },
 }: Props) {
   return (
-    <Link href={slug}>
-      <li className="py-4 flex gap-8">
-        <time className="shrink-0 text-sm">{formatDate(createdAt)}</time>
+    <li>
+      <Link
+        href={slug}
+        className="group hover:bg-gray-200 dark:hover:bg-gray-700 group px-2 py-4 flex gap-8"
+      >
+        <time className="shrink-0 text-sm leading-8">
+          {formatDate(createdAt)}
+        </time>
         <section className="grow overflow-hidden flex flex-col gap-3">
-          <h3 className="font-bold text-xl truncate">{title}</h3>
+          <h3 className="group-hover:underline decoration-indigo-500 underline-offset-6 leading-8 font-bold text-xl truncate">
+            {title}
+          </h3>
           <p className="truncate">{description}</p>
         </section>
-      </li>
-    </Link>
+      </Link>
+    </li>
   );
 }
