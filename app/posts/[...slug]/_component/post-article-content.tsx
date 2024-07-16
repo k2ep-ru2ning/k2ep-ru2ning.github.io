042aff2ca86cf89114fa0cad64ad663961d745e1
k2ep-ru2ning <rookie2000g@gmail.com>
2024-07-16 11:38:49 +0900

    feat: markdown viewer에 remarkGfm 플러그인 추가

--- app/posts/[...slug]/_component/post-article-content.tsx
+++ app/posts/[...slug]/_component/post-article-content.tsx
@@ -1,12 +1,17 @@
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 type Props = {
   contentAsMarkdown: string;
 };
 
 export default function PostArticleContent({ contentAsMarkdown }: Props) {
+  const remarkPlugins = [remarkGfm];
   return (
-    <ReactMarkdown className="prose dark:prose-invert max-w-full">
+    <ReactMarkdown
+      className="prose dark:prose-invert max-w-full"
+      remarkPlugins={remarkPlugins}
+    >
       {contentAsMarkdown}
     </ReactMarkdown>
   );
