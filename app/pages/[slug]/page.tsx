79d099928e17f34ff02cb3a3d24410a838f1d52b
k2ep-ru2ning <rookie2000g@gmail.com>
2024-08-08 23:03:21 +0900

    feat: 전체 글 목록을 페이지 단위로 잘라서 표시하는 기능 구현

--- app/pages/[slug]/page.tsx
+++ app/pages/[slug]/page.tsx
@@ -8,6 +8,8 @@ type Props = {
   };
 };
 
+const PAGE_SIZE = 5;
+
 export default async function PostsPage({ params: { slug } }: Props) {
   if (!/^\d+$/.test(slug)) {
     notFound();
@@ -15,11 +17,26 @@ export default async function PostsPage({ params: { slug } }: Props) {
 
   const posts = await getSortedPosts();
 
+  const currentPageNumber = Number(slug);
+
+  const numberOfPages = Math.ceil(posts.length / PAGE_SIZE);
+
+  if (currentPageNumber < 1 || currentPageNumber > numberOfPages) {
+    notFound();
+  }
+
+  const firstPostIndexOfCurrentPage = (currentPageNumber - 1) * PAGE_SIZE;
+
+  const postsOfCurrentPage = posts.slice(
+    firstPostIndexOfCurrentPage,
+    firstPostIndexOfCurrentPage + PAGE_SIZE,
+  );
+
   return (
     <section className="py-3 md:py-4 flex flex-col gap-2">
-      <h2 className="font-bold text-2xl">{`Page ${slug}`}</h2>
+      <h2 className="font-bold text-2xl">{`Page ${currentPageNumber}`}</h2>
       <ul className="divide-y divide-gray-500 dark:divide-gray-400">
-        {posts.map((post) => (
+        {postsOfCurrentPage.map((post) => (
           <PostListItem key={post.path} post={post} />
         ))}
       </ul>
