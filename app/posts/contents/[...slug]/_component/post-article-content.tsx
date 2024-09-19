0ce986810966c0738a7983691b801106896a4235
k2ep-ru2ning <rookie2000g@gmail.com>
2024-09-19 20:12:12 +0900

    feat: rehype-pretty-code를 이용해 code 태그 스타일링

--- app/posts/contents/[...slug]/_component/post-article-content.tsx
+++ app/posts/contents/[...slug]/_component/post-article-content.tsx
@@ -1,41 +1,27 @@
-import cn from "@/app/_lib/cn";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import { HTMLAttributes } from "react";
 import remarkGfm from "remark-gfm";
+import rehypePrettyCode, {
+  Options as RehypePrettyCodeOptions,
+} from "rehype-pretty-code";
 
 type Props = {
   contentAsMarkdown: string;
 };
 
+const rehypePrettyCodeOptions: RehypePrettyCodeOptions = {
+  theme: "tokyo-night",
+};
+
 export default function PostArticleContent({ contentAsMarkdown }: Props) {
-  const remarkPlugins = [remarkGfm];
-
-  const Code = ({ children, ...props }: HTMLAttributes<HTMLElement>) => {
-    const match = /language-(\w+)/.exec(props.className ?? "");
-    if (match) {
-      return <code {...props}>{children}</code>;
-    } else {
-      return (
-        <code
-          {...props}
-          className={cn(
-            props.className,
-            "before:content-[none] after:content-[none] inline-block px-1.5 rounded-md bg-zinc-100 dark:bg-zinc-800 text-indigo-500",
-          )}
-        >
-          {children}
-        </code>
-      );
-    }
-  };
-
   return (
     <div className="prose dark:prose-invert max-w-full">
       <MDXRemote
         source={contentAsMarkdown}
-        options={{ mdxOptions: { remarkPlugins } }}
-        components={{
-          code: Code,
+        options={{
+          mdxOptions: {
+            remarkPlugins: [remarkGfm],
+            rehypePlugins: [[rehypePrettyCode, rehypePrettyCodeOptions]],
+          },
         }}
       />
     </div>
