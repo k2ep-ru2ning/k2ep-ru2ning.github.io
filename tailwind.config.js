2d238948083208d9ce1ef2caf7654bbaed764c82
k2ep-ru2ning <rookie2000g@gmail.com>
2024-09-24 00:06:11 +0900

    feat: inline code block 스타일링

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  darkMode: "selector",
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "code:not([data-language])": {
+              display: "inline-block",
+              "background-color": theme("colors.zinc.100"),
+              "border-radius": theme("borderRadius.md"),
+              "padding-left": theme("spacing[1.5]"),
+              "padding-right": theme("spacing[1.5]"),
+              color: theme("colors.indigo.500"),
+              "&::before": {
+                display: "none",
+              },
+              "&::after": {
+                display: "none",
+              },
+            },
+            ".dark code:not([data-language])": {
+              "background-color": theme("colors.zinc.800"),
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
+
+export default config;
