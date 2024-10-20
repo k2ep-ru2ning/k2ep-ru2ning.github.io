c0be2c0ba9501af05835aa9ee1d7ab7f2e49fd33
k2ep-ru2ning <rookie2000g@gmail.com>
2024-10-20 14:38:44 +0900

    refactor: Tooltip.Provider 위치를 Tooltip을 사용하는 부분만 감싸도록 수정

--- app/_component/header.tsx
+++ app/_component/header.tsx
@@ -1,3 +1,4 @@
+import * as Tooltip from "@radix-ui/react-tooltip";
 import IconLinks from "./links/icon-links";
 import LogoLink from "./links/logo-link";
 import ThemeSwitchButton from "./theme-switch-button";
@@ -7,8 +8,10 @@ export default function Header() {
     <header className="flex items-center justify-between py-4">
       <LogoLink />
       <div className="flex gap-3">
-        <IconLinks />
-        <ThemeSwitchButton />
+        <Tooltip.Provider delayDuration={400} skipDelayDuration={100}>
+          <IconLinks />
+          <ThemeSwitchButton />
+        </Tooltip.Provider>
       </div>
     </header>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { type ReactNode } from "react";
 import pretendard from "./_font/pretendard";
 import Header from "./_component/header";
 import Footer from "./_component/footer";
-import * as Tooltip from "@radix-ui/react-tooltip";
 import initializeThemeFromLocal from "./_util/initialize-theme-from-local";
 
 type Props = {
@@ -24,13 +23,11 @@ export default function RootLayout({ children }: Props) {
       <body
         className={`${pretendard.className} bg-zinc-50 dark:bg-zinc-950 text-zinc-950 dark:text-zinc-50`}
       >
-        <Tooltip.Provider delayDuration={400} skipDelayDuration={100}>
-          <div className="px-4 max-w-screen-md mx-auto min-h-dvh flex flex-col">
-            <Header />
-            <div className="px-2 grow">{children}</div>
-            <Footer />
-          </div>
-        </Tooltip.Provider>
+        <div className="px-4 max-w-screen-md mx-auto min-h-dvh flex flex-col">
+          <Header />
+          <div className="px-2 grow">{children}</div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
