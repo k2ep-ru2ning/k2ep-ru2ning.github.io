22c490d99d95ad09881dbf5e22bf20e080d81a6d
k2ep-ru2ning <rookie2000g@gmail.com>
2024-10-28 21:49:47 +0900

    fix: (next 개발 서버로 로컬에서 개발할 때) "output: export"로 설정했을 때, generateStaticParams 관련 에러가 발생하는 문제를 빌드할 때만 "output: export"로 설정해서 문제 해결
    - 발생한 에러 예시
    
    Error: Page "/tags/[tag]/pages/[page-number]/page" is missing param "/tags/project-%ED%9A%8C%EA%B3%A0/pages/1" in "generateStaticParams()", which is required with "output: export" config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  output: "export",
+  output: isProd ? "export" : undefined,
   images: {
     unoptimized: true,
   },
