e103d3972d8279cf94bb9ed78abae49394b9a52c
k2ep-ru2ning <rookie2000g@gmail.com>
2024-11-03 19:50:01 +0900

    chore: jetbrains mono font를 local font로 사용하지 않고 next에서 지원하는 google font를 이용해서 사용

--- app/_font/jetbrains-mono.ts
+++ app/_font/jetbrains-mono.ts
@@ -1,88 +1,10 @@
-import localFont from "next/font/local";
+import { JetBrains_Mono } from "next/font/google";
 
-const jetbrainsMono = localFont({
-  src: [
-    {
-      path: "./JetBrainsMono-Thin.woff2",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "./JetBrainsMono-ExtraLight.woff2",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "./JetBrainsMono-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "./JetBrainsMono-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./JetBrainsMono-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./JetBrainsMono-SemiBold.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./JetBrainsMono-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "./JetBrainsMono-ExtraBold.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "./JetBrainsMono-ThinItalic.woff2",
-      weight: "100",
-      style: "italic",
-    },
-    {
-      path: "./JetBrainsMono-ExtraLightItalic.woff2",
-      weight: "200",
-      style: "italic",
-    },
-    {
-      path: "./JetBrainsMono-LightItalic.woff2",
-      weight: "300",
-      style: "italic",
-    },
-    {
-      path: "./JetBrainsMono-Italic.woff2",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "./JetBrainsMono-MediumItalic.woff2",
-      weight: "500",
-      style: "italic",
-    },
-    {
-      path: "./JetBrainsMono-SemiBoldItalic.woff2",
-      weight: "600",
-      style: "italic",
-    },
-    {
-      path: "./JetBrainsMono-BoldItalic.woff2",
-      weight: "700",
-      style: "italic",
-    },
-    {
-      path: "./JetBrainsMono-ExtraBoldItalic.woff2",
-      weight: "800",
-      style: "italic",
-    },
-  ],
+const jetbrainsMono = JetBrains_Mono({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
   variable: "--font-jetbrains-mono",
 });
 
