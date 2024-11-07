1f6f18f5a3690f59ad275b5aed06ae070e4870c6
k2ep-ru2ning <rookie2000g@gmail.com>
2024-11-07 15:45:51 +0900

    feat: 모바일 메뉴 다이얼로그 내부에 테마 변경 버튼 이동 및 각 메뉴에 해당하는 아이콘 추가

--- app/_component/header.tsx
+++ app/_component/header.tsx
@@ -9,20 +9,21 @@ import MenuDialog from "./menu-dialog";
 export default function Header() {
   return (
     <header className="sticky top-0 backdrop-blur-sm flex items-center justify-between py-4">
-      <LogoLink />
-      <Tooltip.Provider delayDuration={400} skipDelayDuration={100}>
-        <div className="flex gap-3 h-8 sm:hidden">
-          <MenuDialog />
-          <ThemeSwitchButton />
-        </div>
-        <div className="hidden sm:flex gap-3 h-8">
+      <div className="shrink-0">
+        <LogoLink />
+      </div>
+      <div className="flex gap-3 h-8 sm:hidden">
+        <MenuDialog />
+      </div>
+      <div className="hidden sm:flex gap-3 h-8">
+        <Tooltip.Provider delayDuration={400} skipDelayDuration={100}>
           <Links />
           <VerticalSeparator />
           <ExternalLinks />
           <VerticalSeparator />
           <ThemeSwitchButton />
-        </div>
-      </Tooltip.Provider>
+        </Tooltip.Provider>
+      </div>
     </header>
   );
 }
--- app/_component/menu-dialog.tsx
+++ app/_component/menu-dialog.tsx
@@ -3,10 +3,22 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 import Link from "next/link";
-import { LuAlignJustify, LuX } from "react-icons/lu";
+import {
+  LuAlignJustify,
+  LuExternalLink,
+  LuFileCode,
+  LuHome,
+  LuMoon,
+  LuSun,
+  LuTags,
+  LuX,
+} from "react-icons/lu";
 import HorizontalSeparator from "./separator/horizontal-separator";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
+import cn from "../_lib/cn";
+import { BsGithub } from "react-icons/bs";
+import { toggleTheme } from "../_util/theme";
 
 export default function MenuDialog() {
   const [open, setOpen] = useState(false);
@@ -28,7 +40,7 @@ export default function MenuDialog() {
         <Dialog.Overlay />
         <Dialog.Content className="bg-zinc-50 dark:bg-zinc-950 fixed inset-0 focus:outline-none flex flex-col">
           <header className="flex items-center justify-between p-4">
-            <Dialog.Title className="p-2 font-extrabold text-xl">
+            <Dialog.Title className="p-2 font-extrabold text-xl shrink-0">
               k2ep-ru2ning
             </Dialog.Title>
             <VisuallyHidden.Root asChild>
@@ -44,21 +56,38 @@ export default function MenuDialog() {
           </header>
           <div className="px-4 py-6 flex flex-col gap-y-2 overflow-y-auto grow">
             <ul>
-              <li>
-                <Link href="/" className="block p-2">
-                  홈
-                </Link>
-              </li>
-              <li>
-                <Link href="/posts/pages/1" className="block p-2">
-                  글 목록
-                </Link>
-              </li>
-              <li>
-                <Link href="/tags" className="block p-2">
-                  태그 목록
-                </Link>
-              </li>
+              {[
+                {
+                  link: "/",
+                  icon: <LuHome className="size-5" />,
+                  label: "홈",
+                  isActive: pathname === "/",
+                },
+                {
+                  link: "/posts/pages/1",
+                  icon: <LuFileCode className="size-5" />,
+                  label: "글 목록",
+                  isActive: pathname.startsWith("/posts/pages"),
+                },
+                {
+                  link: "/tags",
+                  icon: <LuTags className="size-5" />,
+                  label: "태그 목록",
+                  isActive: pathname === "/tags",
+                },
+              ].map(({ link, icon, label, isActive }) => (
+                <li key={link}>
+                  <Link
+                    href={link}
+                    className={cn(
+                      "flex gap-2 justify-end items-center p-2 hover:font-bold",
+                      isActive && "text-indigo-500",
+                    )}
+                  >
+                    {icon} {label}
+                  </Link>
+                </li>
+              ))}
             </ul>
             <HorizontalSeparator />
             <ul>
@@ -67,12 +96,24 @@ export default function MenuDialog() {
                   href="https://github.com/k2ep-ru2ning/k2ep-ru2ning.github.io"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="block p-2"
+                  className="flex gap-2 items-center justify-end p-2 hover:font-bold"
                 >
+                  <BsGithub className="size-5" />
                   Github 저장소
+                  <LuExternalLink className="size-5" />
                 </a>
               </li>
             </ul>
+            <HorizontalSeparator />
+            <button
+              type="button"
+              onClick={toggleTheme}
+              className="flex gap-2 items-center justify-end p-2 hover:font-bold"
+            >
+              <LuSun className="dark:hidden size-5" />
+              <LuMoon className="hidden dark:block size-5" />
+              테마 변경
+            </button>
           </div>
         </Dialog.Content>
       </Dialog.Portal>
