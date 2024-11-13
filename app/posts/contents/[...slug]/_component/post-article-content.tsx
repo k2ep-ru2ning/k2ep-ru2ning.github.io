2b8e080bdb06d04c2fe9afedb6788ae559a30c46
k2ep-ru2ning <rookie2000g@gmail.com>
2024-11-13 14:27:06 +0900

    chore: next-mdx-remote 패키지 대신, mdx-bundler 패키지를 사용해서 mdx를 rendering 가능한 컴포넌트로 변환

--- app/posts/contents/[...slug]/_component/post-article-content.tsx
+++ app/posts/contents/[...slug]/_component/post-article-content.tsx
@@ -1,10 +1,11 @@
-import { MDXRemote } from "next-mdx-remote/rsc";
 import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
 import rehypePrettyCode, {
   Options as RehypePrettyCodeOptions,
 } from "rehype-pretty-code";
 import RoundedImage from "./rounded-image";
+import { bundleMDX } from "mdx-bundler";
+import { getMDXComponent } from "mdx-bundler/client";
 
 type Props = {
   contentAsMarkdown: string;
@@ -17,19 +18,30 @@ const rehypePrettyCodeOptions: RehypePrettyCodeOptions = {
   },
 };
 
-export default function PostArticleContent({ contentAsMarkdown }: Props) {
+export default async function PostArticleContent({ contentAsMarkdown }: Props) {
+  const { code } = await bundleMDX({
+    source: contentAsMarkdown,
+    mdxOptions(options) {
+      options.remarkPlugins = [
+        ...(options.remarkPlugins ?? []),
+        remarkGfm,
+        remarkBreaks,
+      ];
+      options.rehypePlugins = [
+        ...(options.rehypePlugins ?? []),
+        [rehypePrettyCode, rehypePrettyCodeOptions],
+      ];
+      return options;
+    },
+  });
+
+  const MDXComponent = getMDXComponent(code);
+
   return (
     <div className="prose prose-zinc dark:prose-invert prose-sm sm:prose-base prose-code:text-sm sm:prose-code:text-base max-w-full">
-      <MDXRemote
-        source={contentAsMarkdown}
+      <MDXComponent
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         components={{ img: RoundedImage as any }}
-        options={{
-          mdxOptions: {
-            remarkPlugins: [remarkGfm, remarkBreaks],
-            rehypePlugins: [[rehypePrettyCode, rehypePrettyCodeOptions]],
-          },
-        }}
       />
     </div>
   );
