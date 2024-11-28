8106a4f9b113363357c20d4a828904d60f90d363
k2ep-ru2ning <rookie2000g@gmail.com>
2024-11-28 17:21:24 +0900

    fix: header에 가려서 tooltip이 제대로 표시 되지 않는 문제 해결
    -tooltip의 z-index도 header와 동일하게 설정

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -19,7 +19,7 @@ export default function Tooltip({ trigger, text }: Props) {
         <TooltipPrimitive.Content
           side="bottom"
           sideOffset={8}
-          className="text-sm bg-zinc-200 dark:bg-zinc-700 py-1 px-2 rounded-md"
+          className="z-10 text-sm bg-zinc-200 dark:bg-zinc-700 py-1 px-2 rounded-md"
         >
           {text}
           <TooltipPrimitive.Arrow className="fill-zinc-200 dark:fill-zinc-700" />
