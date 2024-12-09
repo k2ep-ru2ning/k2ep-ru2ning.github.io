1b10ebf306fc053e0b86b9df79b56175b156dbf8
k2ep-ru2ning <rookie2000g@gmail.com>
2024-12-09 23:56:34 +0900

    chore: hero 영역 스타일링 수정

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <section className="text-lg sm:text-xl py-8 sm:py-10 text-center">
+    <section className="text-xl py-16 text-center">
       <p>
         프로그래밍 관련 <strong className="text-indigo-500">지식/경험</strong>을
         <br />
