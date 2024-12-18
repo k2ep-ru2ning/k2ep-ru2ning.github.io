ccbd1f353e629c723f9b40ed05ff2339133489cc
k2ep-ru2ning <rookie2000g@gmail.com>
2024-12-18 23:54:57 +0900

    chore: tooltip hover시 content가 바로 표시되도록 delayDuration 설정 변경

--- src/components/tooltip/tooltip-provider.tsx
+++ src/components/tooltip/tooltip-provider.tsx
@@ -3,7 +3,7 @@ import { type PropsWithChildren } from "react";
 
 export default function TooltipProvider({ children }: PropsWithChildren) {
   return (
-    <TooltipPrimitive.Provider delayDuration={400} skipDelayDuration={100}>
+    <TooltipPrimitive.Provider delayDuration={100} skipDelayDuration={100}>
       {children}
     </TooltipPrimitive.Provider>
   );
