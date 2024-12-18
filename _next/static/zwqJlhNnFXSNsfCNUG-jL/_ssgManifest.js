de26c776c9493e942f362e8c8e25ba92b236d636
k2ep-ru2ning <rookie2000g@gmail.com>
2024-12-18 23:56:54 +0900

    Updates

---
+++ _next/static/zwqJlhNnFXSNsfCNUG-jL/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fposts\u002Fcontents\u002F[...slug]","\u002Fposts\u002Fpages\u002F[page-number]","\u002Fseries\u002F[name]","\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
