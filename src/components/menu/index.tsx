0537f9c88be1ed2da4b4c3a2a08dc0a4b89e624b
k2ep-ru2ning <rookie2000g@gmail.com>
2024-12-26 15:34:05 +0900

    feat: 탭 간의 테마 동기화
    - 다른 탭에서 local storage의 theme 값을 변경한 경우, dark class 값을 추가/제거해 테마 토글

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import useThemeSync from "@/hooks/use-theme-sync";
 import DesktopMenu from "./desktop-menu";
 import MobileMenu from "./mobile-menu";
 
 export default function Menu() {
+  useThemeSync();
+
   return (
     <>
       <MobileMenu />
