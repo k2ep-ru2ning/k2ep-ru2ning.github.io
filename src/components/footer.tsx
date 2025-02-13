e71e473ca6baf9694a9ea1d125ef192104ee69e6
k2ep-ru2ning <rookie2000g@gmail.com>
2025-02-13 14:27:36 +0900

    feat: footer의 년도가 빌드할 때마다 변경되도록 수정

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="h-[80px] flex flex-col justify-center items-center">
-      <p>&copy; 2024 k2ep-ru2ning</p>
+      <p>&copy; {year} k2ep-ru2ning</p>
     </footer>
   );
 }
