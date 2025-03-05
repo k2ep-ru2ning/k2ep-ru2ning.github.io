858eb21d92c8b044904c6a63efa4298d6c81027a
k2ep-ru2ning <rookie2000g@gmail.com>
2025-03-05 22:24:14 +0900

    chore: 홈 페이지 스타일 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 import Link from "next/link";
+import { LuArrowRight } from "react-icons/lu";
 import Heading from "@/components/heading";
 import Section from "@/components/section";
 
 export default function HomePage() {
   return (
-    <Section>
+    <Section className="gap-10 sm:gap-12">
       <header className="flex flex-col gap-1">
         <Heading as="h2">
           k2ep-ru2ning&apos;s <span className="text-indigo-500">Tech Blog</span>
         </Heading>
         <div className="flex flex-col sm:flex-row sm:gap-1 text-sm text-zinc-700 dark:text-zinc-300">
           <div className="before:content-['·'] sm:before:content-none flex gap-1">
-            Jongmin Kim
-          </div>
-          <div className="before:content-['·'] flex gap-1">
             Software Engineer
           </div>
           <div className="before:content-['·'] flex gap-1">
@@ -23,13 +21,20 @@ export default function HomePage() {
       </header>
       <div className="flex flex-col gap-4 p-2 rounded-md bg-zinc-200 dark:bg-zinc-800">
         <p>
-          <strong>k2ep-ru2ning</strong>
-          이라는 이름처럼 <strong>즐겁게 꾸준히 개발하고 싶습니다.</strong>
+          <span className="font-bold">k2ep-ru2ning</span>
+          이라는 이름처럼{" "}
+          <span className="font-bold">즐겁게 꾸준히 개발하고 싶습니다.</span>
         </p>
-        <p>개발하면서 배우고 경험한 내용을 정리하는 것을 좋아합니다.</p>
         <p>
-          요즘에는 JavaScript와 React를 공부하면서 주로 Front-end 개발을 하고
-          있습니다.
+          <span className="font-bold">
+            개발하면서 배우고 경험한 내용을 정리하는 것
+          </span>
+          을 좋아합니다.
+        </p>
+        <p>
+          요즘에는 <span className="font-bold">JavaScript</span>와{" "}
+          <span className="font-bold">React</span>를 공부하면서 주로{" "}
+          <span className="font-bold">Front-end</span> 개발을 하고 있습니다.
         </p>
       </div>
       <ul className="flex flex-col gap-2 sm:gap-0">
@@ -49,12 +54,13 @@ export default function HomePage() {
             key={index}
             className="flex flex-col items-start sm:flex-row sm:gap-3 sm:items-center"
           >
-            <div className="w-14">
+            <div className="w-20">
               <Link
                 href={link}
-                className="p-1 rounded-md hover:bg-zinc-200 dark:hover:bg-zinc-700"
+                className="inline-flex gap-1 items-center p-1 rounded-md hover:bg-zinc-200 dark:hover:bg-zinc-700"
               >
                 {label}
+                <LuArrowRight className="size-4" />
               </Link>
             </div>
             <p className="p-1 text-sm text-zinc-700 dark:text-zinc-300">
