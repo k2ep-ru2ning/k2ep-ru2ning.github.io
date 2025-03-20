29f1ec1c7d4dcd96986feed817adc67c84b7392d
k2ep-ru2ning <rookie2000g@gmail.com>
2025-03-21 00:16:26 +0900

    chore: 글 상세 페이지 h2, h3 태그 스타일링 수정

--- src/components/post-article/content/post-article-content-heading.tsx
+++ src/components/post-article/content/post-article-content-heading.tsx
@@ -17,7 +17,10 @@ export default function PostArticleContentHeading({
     as, // h2, h3에 반복되는 속성이 많아 코드 반복을 줄이기 위해, JSX 대신 createElement 함수를 사용
     { id, className: "scroll-mt-20" },
     id ? (
-      <Link href={`#${id}`} className="inline-block group no-underline">
+      <Link
+        href={`#${id}`}
+        className="font-bold inline-block group no-underline"
+      >
         {children}
         {/* :focus-visible은 오직 키보드를 통해서 focus 된 요소를 선택, 즉 마우스 클릭이나 스크린 터치로 요소에 focus를 준 경우는 선택 안함 */}
         <div className="opacity-0 group-hover:opacity-100 group-focus-visible:opacity-100 ml-1 inline-flex justify-center items-center p-0.5 sm:p-1 rounded-md border border-zinc-300 dark:border-zinc-700 hover:bg-zinc-200 dark:hover:bg-zinc-700">
