23e456393b33fc70410b6e575696f1967dddcfa5
jongmin kim <k2epru2ning@gmail.com>
2025-08-11 16:54:20 +0900

    feat: bg-zinc-*로 배경색 표현하던 것을 bg-background로 변경(shadcn의 css variable 활용)

--- src/components/post-article/scroll-to-top-button.tsx
+++ src/components/post-article/scroll-to-top-button.tsx
@@ -16,7 +16,7 @@ export default function ScrollToTopButton({ size }: Props) {
     <button
       type="button"
       onClick={handleClickScrollToTopButton}
-      className="bg-zinc-50 dark:bg-zinc-950 rounded-md border border-zinc-300 dark:border-zinc-700 p-1 hover:bg-zinc-200 dark:hover:bg-zinc-700"
+      className="bg-background rounded-md border border-zinc-300 dark:border-zinc-700 p-1 hover:bg-zinc-200 dark:hover:bg-zinc-700"
     >
       <LuArrowUpToLine className={cn("size-5", size === "large" && "size-7")} />
     </button>
