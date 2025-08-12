1169ee9fa05b682129f9a5355f7c5774339b6f45
jongmin kim <k2epru2ning@gmail.com>
2025-08-13 00:07:58 +0900

    feat: MobileMenu 컴포넌트에 ScrollArea 적용

--- src/components/menu/mobile-menu.tsx
+++ src/components/menu/mobile-menu.tsx
@@ -25,6 +25,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
+import { ScrollArea } from "../ui/scroll-area";
 import { Separator } from "../ui/separator";
 
 export default function MobileMenu() {
@@ -53,9 +54,9 @@ export default function MobileMenu() {
       </DialogTrigger>
       <DialogContent
         showCloseButton={false}
-        className="p-0 inset-0 translate-0 flex flex-col rounded-none max-w-full"
+        className="duration-0 p-0 inset-0 translate-0 rounded-none max-w-full w-full h-dvh block"
       >
-        <header className="flex items-center justify-between p-3">
+        <header className="flex items-center justify-between p-3 h-[64px]">
           <DialogTitle className="px-2 py-1 font-extrabold text-xl shrink-0">
             메뉴
           </DialogTitle>
@@ -68,76 +69,78 @@ export default function MobileMenu() {
             </Button>
           </DialogClose>
         </header>
-        <div className="px-4 py-6 flex flex-col gap-y-2 overflow-y-auto grow">
-          <ul>
-            {[
-              {
-                link: "/",
-                icon: <House className="size-5" />,
-                label: "홈",
-                isActive: pathname === "/",
-              },
-              {
-                link: "/posts",
-                icon: <File className="size-5" />,
-                label: "글",
-                isActive: pathname === "/posts",
-              },
-              {
-                link: "/series",
-                icon: <FileStack className="size-5" />,
-                label: "시리즈",
-                isActive: pathname === "/series",
-              },
-            ].map(({ link, icon, label, isActive }) => (
-              <li key={link}>
+        <ScrollArea className="h-[calc(100%-64px)] overflow-hidden px-4 pb-4">
+          <div className="flex flex-col gap-2">
+            <ul>
+              {[
+                {
+                  link: "/",
+                  icon: <House className="size-5" />,
+                  label: "홈",
+                  isActive: pathname === "/",
+                },
+                {
+                  link: "/posts",
+                  icon: <File className="size-5" />,
+                  label: "글",
+                  isActive: pathname === "/posts",
+                },
+                {
+                  link: "/series",
+                  icon: <FileStack className="size-5" />,
+                  label: "시리즈",
+                  isActive: pathname === "/series",
+                },
+              ].map(({ link, icon, label, isActive }) => (
+                <li key={link}>
+                  <Button
+                    variant="ghost"
+                    className={cn(
+                      "flex gap-2 justify-center items-center",
+                      isActive && "text-indigo-500 hover:text-indigo-500",
+                    )}
+                    asChild
+                  >
+                    <Link href={link}>
+                      {icon} {label}
+                    </Link>
+                  </Button>
+                </li>
+              ))}
+            </ul>
+            <Separator />
+            <ul>
+              <li>
                 <Button
-                  variant="ghost"
-                  className={cn(
-                    "flex gap-2 justify-center items-center",
-                    isActive && "text-indigo-500 hover:text-indigo-500",
-                  )}
                   asChild
+                  variant="ghost"
+                  className="flex gap-2 justify-center items-center"
                 >
-                  <Link href={link}>
-                    {icon} {label}
-                  </Link>
+                  <a
+                    href="https://github.com/k2ep-ru2ning/k2ep-ru2ning.github.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Github className="size-5" />
+                    Github 저장소
+                    <ExternalLink className="size-5" />
+                  </a>
                 </Button>
               </li>
-            ))}
-          </ul>
-          <Separator />
-          <ul>
-            <li>
-              <Button
-                asChild
-                variant="ghost"
-                className="flex gap-2 justify-center items-center"
-              >
-                <a
-                  href="https://github.com/k2ep-ru2ning/k2ep-ru2ning.github.io"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Github className="size-5" />
-                  Github 저장소
-                  <ExternalLink className="size-5" />
-                </a>
-              </Button>
-            </li>
-          </ul>
-          <Separator />
-          <Button
-            type="button"
-            onClick={toggleTheme}
-            variant="ghost"
-            className="flex gap-2 items-center justify-center"
-          >
-            <Sun className="dark:hidden size-5" />
-            <Moon className="hidden dark:block size-5" />
-            테마 변경
-          </Button>
-        </div>
+            </ul>
+            <Separator />
+            <Button
+              type="button"
+              onClick={toggleTheme}
+              variant="ghost"
+              className="flex gap-2 items-center justify-center"
+            >
+              <Sun className="dark:hidden size-5" />
+              <Moon className="hidden dark:block size-5" />
+              테마 변경
+            </Button>
+          </div>
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
