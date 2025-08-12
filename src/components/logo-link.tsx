276dd75d895b1c3ba4f9fa9d4a063d20395d91fa
jongmin kim <k2epru2ning@gmail.com>
2025-08-12 13:34:08 +0900

    feat: LogoLink 컴포넌트에 shadcn button 적용

--- src/components/logo-link.tsx
+++ src/components/logo-link.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function LogoLink() {
   return (
-    <Link
-      href="/"
-      className="flex flex-col text-center px-2 py-1 rounded-md hover:bg-zinc-200 dark:hover:bg-zinc-700"
-    >
-      <h1 className="font-extrabold text-xl">k2ep-ru2ning</h1>
-    </Link>
+    <Button asChild variant="ghost" className="px-2">
+      <Link href="/">
+        <h1 className="font-extrabold text-xl">k2ep-ru2ning</h1>
+      </Link>
+    </Button>
   );
 }
