0be9a2f9a834ae60c795ba2dedd1d736428e711d
jongmin kim <k2epru2ning@gmail.com>
2025-08-12 15:23:35 +0900

    feat: 메인 페이지 shadcn의 테마를 활용해서 글자 색 등을 스타일링, brand color를 css variable 및 tailwindcss의 테마로 등록, 불필요한 css variable 및 tailwindcss 테마 주석 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,33 +10,36 @@ export default function HomePage() {
     <Section className="gap-10 sm:gap-12">
       <header className="flex flex-col gap-1">
         <Heading as="h2">
-          k2ep-ru2ning&apos;s <span className="text-indigo-500">Tech Blog</span>
+          k2ep-ru2ning&apos;s <span className="text-brand">Tech Blog</span>
         </Heading>
-        <div className="flex flex-col sm:flex-row sm:gap-1 text-sm text-zinc-700 dark:text-zinc-300">
-          <div className="before:content-['·'] sm:before:content-none flex gap-1">
+        <div className="flex flex-col sm:flex-row sm:gap-1 text-sm text-secondary-foreground">
+          <span className="before:content-['·'] sm:before:content-none flex gap-1">
             Software Engineer
-          </div>
-          <div className="before:content-['·'] flex gap-1">
+          </span>
+          <span className="before:content-['·'] flex gap-1">
             Front-end Web Developer
-          </div>
+          </span>
         </div>
       </header>
-      <div className="flex flex-col gap-4 p-2 rounded-md bg-zinc-200 dark:bg-zinc-800">
+      <div className="flex flex-col gap-4 p-2 rounded-md bg-secondary text-secondary-foreground">
         <p>
-          <span className="font-bold">k2ep-ru2ning</span>
+          <em className="font-bold not-italic">k2ep-ru2ning</em>
           이라는 이름처럼{" "}
-          <span className="font-bold">즐겁게 꾸준히 개발하고 싶습니다.</span>
+          <em className="font-bold not-italic">
+            즐겁게 꾸준히 개발하고 싶습니다.
+          </em>
         </p>
         <p>
-          <span className="font-bold">
+          <em className="font-bold not-italic">
             개발하면서 배우고 경험한 내용을 정리하는 것
-          </span>
+          </em>
           을 좋아합니다.
         </p>
         <p>
-          요즘에는 <span className="font-bold">JavaScript</span>와{" "}
-          <span className="font-bold">React</span>를 공부하면서 주로{" "}
-          <span className="font-bold">Front-end</span> 개발을 하고 있습니다.
+          요즘에는 <em className="font-bold not-italic">JavaScript</em>와{" "}
+          <em className="font-bold not-italic">React</em>를 공부하면서 주로{" "}
+          <em className="font-bold not-italic">Front-end</em> 개발을 하고
+          있습니다.
         </p>
       </div>
       <ul className="flex flex-col gap-2 sm:gap-0">
@@ -69,7 +72,7 @@ export default function HomePage() {
                 </Link>
               </Button>
             </div>
-            <p className="p-1 text-sm text-zinc-700 dark:text-zinc-300">
+            <p className="p-1 text-sm text-secondary-foreground">
               {description}
             </p>
           </li>
